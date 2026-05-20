7c97b1327cd6265e8a0fb60b8b3899c0a9216d2d
Kly-kly <kleinople60@gmail.com>
2026-05-20 21:13:21 +0800

    Add account module: login, register, layout components

---
+++ account/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AccountService, AlertService } from '../_services';
+
+@Component({
+  templateUrl: './login.component.html',
+  standalone: false
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+
+    if (this.form.invalid) return;
+
+    this.loading = true;
+    this.accountService.login(this.f['email'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: (error: any) => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      });
+  }
+}
